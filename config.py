e2b8c0e89aee538d4dfaae441cda14d0ef59052e
botkomakers <siam420riiya@gmail.com>
2025-05-12 23:16:49 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Example configuration (Make sure to set your bot's token)
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = "APP_ID"
+API_HASH = "API_HASH"
+# Temporary flags
+class Temp:
+    IS_PAUSED = False
+
+temp = Temp()
