7e500cc4325b6ba58e72266b84aeff760c2c7047
botkomakers <siam420riiya@gmail.com>
2025-05-12 23:50:35 +0600

    Update context.py

--- context.py
+++ context.py
@@ -1,5 +1,5 @@
-TOKEN = "YOUR_BOT_TOKEN"
-API_ID = 123456
-API_HASH = "your_api_hash"
-BOT_USERNAME = "YourBotUsername"
-WEBHOOK_URL = "https://your-render-url.onrender.com"
+TOKEN = "6631772048:AAF4AoHOssXJqYMee6oJ_e2C9onB555GipE"
+API_ID = 27752560  # এখানে সঠিক সংখ্যাটি বসাও
+API_HASH = "67d3ec64db8031189962b5d4804884c0"
+BOT_USERNAME = "@your_bot_username"
+WEBHOOK_URL = "https://your-service.onrender.com"
