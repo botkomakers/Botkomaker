6c9bb3a45fe9c7fb042375cab38c08494cdf85ce
botkomakers <siam420riiya@gmail.com>
2025-05-12 23:36:35 +0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Example configuration (Make sure to set your bot's token)
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-API_ID = "APP_ID"
-API_HASH = "API_HASH"
-# Temporary flags
-class Temp:
-    IS_PAUSED = False
-
-temp = Temp()
